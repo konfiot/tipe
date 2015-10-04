fc436358eca4ad28c1cc6fb76edf83eb7222cc54
konfiot <arthurtoussaint@free.fr>
2015-10-04 22:07:25 +0200

    On commence a integrer les quaternions

--- plot_rotation.py
+++ plot_rotation.py
@@ -1,5 +1,7 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+import qmath
 
 class bcolors:
 	HEADER = '\033[95m'
@@ -16,7 +18,9 @@ DATA_FILE = 'tours.csv'
 SEPARATOR = ';'
 
 
-data = []
+QUAT_ROT_INIT = qmath.quaternion(0, [0, 0, 0]) #Init the quaternion to the identity rotation (q = 1 + 0i + 0j + 0k = 1)
+W_ARRAY_INIT = [0, 0, 0]
+TIME_INIT = 0
 
 
 
@@ -28,7 +32,7 @@ def integrate(t, y, origin = 0):
 	return out
 
 
-def get_data(filename, offsets = None):
+def get_cal_data(filename, offsets = None):
 	data = []
 	with open(filename) as file:
 		line = file.readline()
@@ -42,7 +46,6 @@ def detect_sat(d, std_dev, thres=20):
 	c = [0]
 	for i in range(1, len(d)):
 		if abs(d[i - 1] - d[i]) < std_dev:
-		#	print(count)
 			count += 1
 			if count > thres:
 				return True
@@ -50,10 +53,30 @@ def detect_sat(d, std_dev, thres=20):
 			count = 0
 	return False
 
+def norm(array):
+	return math.sqrt(sum([x**2 for x in array]))
+
+def normalize(array):
+	return [x/norm(array) for x in array]
+
+def get_raw_data(f):
+	line = f.readline()
+
+	if not line:
+		return False
+
+
+	data = [float(x.replace(",", ".")) for x in line.split(SEPARATOR)]
+
+	return [
+		data[0], # Time
+		data[1:4], # Rotation velocity
+		data[4:7] # Linear acceleration
+	]
 
 print("Getting calibration data")
 
-cal_data = get_data(CALIBRATION_FILE)
+cal_data = get_cal_data(CALIBRATION_FILE)
 offsets = [0]
 std_dev = [0]
 
@@ -66,21 +89,34 @@ print("Standard deviation", std_dev)
 
 print("Calibration done\n")
 
-print("Detecting saturations")
-data = get_data(DATA_FILE, offsets)
+#print("Detecting saturations")
+#
+#for i in range(1, len(data)):
+#	if detect_sat(data[i], std_dev[i]):
+#		print(bcolors.FAIL + "WARNING : Saturation detected" + bcolors.ENDC)
+#
+#print("End of saturation detection\n")
 
-for i in range(1, len(data)):
-	if detect_sat(data[i], std_dev[i]):
-		print(bcolors.FAIL + "WARNING : Saturation detected" + bcolors.ENDC)
+print("Integrating")
 
-plt.show()
+previous_data = [(TIME_INIT, QUAT_ROT_INIT, W_ARRAY_INIT)]
+terminate = False
 
-print("End of saturation detection\n")
+f = open(DATA_FILE, "r")
+print("Data contains: " + f.readline())
 
-print("Plotting")
+while not terminate:
+	raw_data = get_raw_data(f)
+
+	if not raw_data: # Escape the loop if there's no more data
+		terminate = True
+		continue
+
+	p_t, p_quat, p_w = previous_data[-1] # Get the old data
+	new_quat_rot = qmath.quaternion(norm(raw_data[1]) * (raw_data[0] - p_t), normalize([raw_data[1][i] - offsets[i] for i in range(len(raw_data[1]))])) # Calculate the new quaternion
+
+	previous_data.append((raw_data[0], p_quat * new_quat_rot, raw_data[1])) # Add the new data to the old
 
 
-for d in data[1:]:
-	plt.plot(data[0], d)
-	plt.plot(data[0], integrate(data[0], d))
+
 plt.show()
