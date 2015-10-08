4fae6ceb9dd0ce5a9fdbf71062375bc62b5aadd0
konfiot <arthurtoussaint@free.fr>
2015-10-08 21:56:48 +0200

    On change de fournisseur (de quaternions)

--- plot_rotation.py
+++ plot_rotation.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+
 import socket
 from math import sqrt, atan2, asin, pi
 import numpy as np
 import matplotlib.pyplot as plt
-import qmath
+from mathutils import Quaternion
 
 class bcolors:
 	HEADER = '\033[95m'
@@ -17,9 +19,9 @@ class bcolors:
 CALIBRATION_FILE = "cal.csv"
 DATA_FILE = 'rot90.csv'
 SEPARATOR = ','
+TIMEFRAME = 10
 
-
-QUAT_ROT_INIT = qmath.quaternion(0, [0, 0, 0]) #Init the quaternion to the identity rotation (q = 1 + 0i + 0j + 0k = 1)
+QUAT_ROT_INIT = Quaternion([1, 0, 0, 0]) #Init the quaternion to the identity rotation (q = 1 + 0i + 0j + 0k = 1)
 W_ARRAY_INIT = [0, 0, 0]
 TIME_INIT = 0
 
@@ -46,7 +48,7 @@ def detect_sat(d, std_dev, thres=20):
 	count = 0
 	c = [0]
 	for i in range(1, len(d)):
-		if abs(d[i - 1] - d[i]) < std_dev:
+		if abs(d[i - 1] - d[i]) < std_dev/10:
 			count += 1
 			if count > thres:
 				return True
@@ -62,6 +64,7 @@ def normalize(array):
 
 def get_raw_data(f):
 	line, _ = f.recvfrom(8192)
+	line = line.decode("ascii")
 
 	if not line:
 		return False
@@ -109,9 +112,11 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind(("", 5555))
 
 plt.ion()
-plt.show()
 fig, ax = plt.subplots()
 lines = ax.plot([],[],[],[],[],[])
+
+ax.legend(lines, ["phi", "theta", "psi"])
+plt.show()
 
 ax.set_ylim(-pi, pi)
 
@@ -135,33 +140,32 @@ while not terminate:
 
 
 	p_t, p_quat, p_w = previous_data[-1] # Get the old data
-	new_quat_rot = qmath.quaternion(norm(raw_data[1]) * (raw_data[0] - t[0] - p_t), normalize(raw_data[1])) # Calculate the new quaternion
+	new_quat_rot = Quaternion(normalize(raw_data[1]), norm(raw_data[1]) * (raw_data[0] - t[0] - p_t)) # Calculate the new quaternion
 
 	q = p_quat * new_quat_rot
 
 	previous_data.append((raw_data[0] - t[0], q, raw_data[1])) # Add the new data to the old
 
-	angles[0].append(atan2(2*q[0]*q[1] + q[2]*q[3], 1 - 2*(q[1]**2 + q[2]**2)))
-	angles[1].append(asin(2*(q[0] * q[2] - q[3] * q[1])))
-	angles[2].append(atan2(2*q[0]*q[3] + q[1]*q[2], 1 - 2*(q[2]**2 + q[3]**2)))
+	phi, theta, psi = q.to_euler()
+	angles[0].append(phi) # Compute Euler angles and display them
+	angles[1].append(theta)
+	angles[2].append(psi)
+	print("%.4f" % angles[0][-1] + "\t", end="")
+	print("%.4f" % angles[1][-1] + "\t", end="")
+	print("%.4f" % angles[2][-1])
 
-	print("%.5f" % angles[0][-1]) + "\t",
-	print("%.5f" % angles[1][-1]) + "\t",
-	print("%.5f" % angles[2][-1])
+	#a = q.inverse() * qmath.quaternion([0] + raw_data[1]) * q
 
+	
+
+# Display dara
 	c += 1
-
 	if c%5 == 0:
 		for i in range(len(lines)):
 			lines[i].set_data(t, angles[i])
 		plt.draw()
-		ax.set_xlim(t[0], raw_data[0])
+		ax.set_xlim(max(t[0], raw_data[0]-TIMEFRAME), raw_data[0])
 
 plt.ioff()
 plt.show()
-
-
-t = [v[0] for v in previous_data]
-quat = [v[1] for v in previous_data]
-
 
