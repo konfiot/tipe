e6cc1a53231f033bf1752493b75e83bb0b4dc86f
konfiot <arthurtoussaint@free.fr>
2015-11-08 10:31:03 +0100

    L'optimisation quaternionique marche bien

--- plot_rotation.py
+++ plot_rotation.py
@@ -5,6 +5,7 @@ from math import sqrt, atan2, asin, pi
 import numpy as np
 import matplotlib.pyplot as plt
 from mathutils import Quaternion
+import math
 
 class bcolors:
 	HEADER = '\033[95m'
@@ -23,6 +24,7 @@ SEPARATOR = ','
 TIMEFRAME = 10
 
 QUAT_ROT_INIT = Quaternion([1, 0, 0, 0]) #Init the quaternion to the identity rotation (q = 1 + 0i + 0j + 0k = 1)
+QUAT_MAG_AIM = Quaternion([0, 20.8e-3, 0.0696e-3, 43.3e-3])
 W_ARRAY_INIT = [0, 0, 0]
 TIME_INIT = 0
 
@@ -48,6 +50,9 @@ def detect_sat(d, std_dev, thres=20):
 			count = 0
 	return False
 
+def inv_sqrt(x):
+	return 1/np.sqrt(x)
+
 def norm(array):
 	return sqrt(sum([x**2 for x in array]))
 
@@ -66,8 +71,29 @@ def get_raw_data(f):
 	return [
 		data[0], # Time
 		data[6:9], # Rotation velocity
-		data[2:5] # Linear acceleration
+		data[2:5], # Linear acceleration
+		data[10:13]
 	]
+
+def jacobian(q, d):
+	return np.matrix([	[0, 0, 0, 0],
+				[2 * d[2] * q[3] - 2 * d[3] * q[2], 2 * d[2] * q[2] - 2 * d[3] * q[3], -4 * d[1] * q[2] + 2 * d[2] * q[1] - 2 * d[3] * q[0], -4 * d[0] * q[3] + 2 * d[2] * q[0] - 2 * d[3] * q[1]],
+				[-2 * d[1] * q[3] + 2 * d[3] * q[1], 2 * d[1] * q[2] - 4 * d[2] * q[1] + 2 * d[3] * q[0], 2 * d[1] * q[2] + 2 * d[3] * q[1], -2 * d[1] * q[1] - 4 * d[2] * q[3] + 2 * d[3] * q[2]],
+				[2 * d[1] * q[2] - 2 * d[2] * q[1], 2 * d[1] * q[3] - 2 * d[2] * q[0] - 4 * d[3] * q[1], 2 * d[1] * q[0] + 2 * d[2] * q[3] - 4 * d[3] * q[2], 2 * d[1] * q[1] + 2 * d[2] * q[2]]])
+
+def quat_from_static(a, b, a_aim, b_aim, old, dt):
+	f_a = old.inverted()*a_aim*old - a_aim
+	f_b = old.inverted()*b_aim*old - b_aim
+
+	j_a = jacobian(a, a_aim)
+	j_b = jacobian(b, b_aim)
+
+
+	f = np.bmat([[list(f_a)], [list(f_b)]]).transpose()
+	j = np.bmat([[j_a.transpose(), j_b.transpose()]])
+
+	delta_f = np.dot(j, f)
+	return old - dt * Quaternion(delta_f).normalized()
 
 print("Getting calibration data")
 
@@ -120,6 +146,8 @@ c = 0
 previous_data = [(TIME_INIT, QUAT_ROT_INIT, W_ARRAY_INIT)]
 terminate = False
 
+q_static = QUAT_ROT_INIT
+
 while not terminate:
 	raw_data = get_raw_data(s)
 
@@ -142,19 +170,22 @@ while not terminate:
 
 	q = p_quat * new_quat_rot
 
+	q_static = quat_from_static(Quaternion([0] + raw_data[2]).normalized(), Quaternion([0, 0, 0, 1]), Quaternion([0] + raw_data[3]).normalized(), QUAT_MAG_AIM.normalized(), q_static, raw_data[0] - t[0] - p_t).normalized()
+
+
 	previous_data.append((raw_data[0] - t[0], q, raw_data[1])) # Add the new data to the old
 
-	phi, theta, psi = q.to_euler()
+	phi, theta, psi = q_static.to_euler()
 	angles[0].append(phi) # Compute Euler angles
 	angles[1].append(theta)
 	angles[2].append(psi)
 
 
-	a = q * Quaternion([0] + raw_data[2]) * q.inverted() # Get acceleration in the fixed reference frame
+	a = q_static * Quaternion([0] + raw_data[2]) * q_static.inverted() # Get acceleration in the fixed reference frame
 	a[3] -= 9.813
 	for i in range(len(acc)):
 		acc[i].append(a[i+1])
-	print(a)
+
 
 
 
